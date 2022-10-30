5ede7626af1716924e62e3ef68dfcd49d66da358
Камиль Радов <QBCi321@gmail.com>
2022-10-30 23:42:46 +0300

    4

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'InfraAppConfig'
+    'infra_app.apps.InfraAppConfig'
 ]
 
 MIDDLEWARE = [
